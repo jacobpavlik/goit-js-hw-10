26d10a584201ffe37c91046b9305331bd10c1d37
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-05-21 12:36:01 +0200

    added html elements

--- src/index.js
+++ src/index.js
@@ -14,18 +14,40 @@ console.log(countryList);
 function handleInput() {
   let countryName = 'Poland';
   countryName = searchBox.value;
+  if (countryName === '') {
+    return;
+  }
   let countryPath = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
   console.log(countryName);
   console.log(countryPath);
   fetchCountries(countryPath)
     .then(countries => {
+      countryInfo.innerHTML = '';
+      countryList.innerHTML = '';
       console.log('z index data', countries);
       if (countries.length === 1) {
+        countryInfo.innerHTML = '';
         countries.forEach((country, countryIndex) => {
+          countryInfo.insertAdjacentHTML(
+            'beforeend',
+            `<img src="${countries[countryIndex].flags.svg}" alt="${
+              countries[countryIndex].flags.alt
+            }" width="90" height="57"/><span>${
+              countries[countryIndex].name.official
+            }</span>
+          <p>Capital: ${countries[countryIndex].capital}</p>
+          <p>Population: ${countries[countryIndex].population}</p>
+          <p>Language: ${Object.values(countries[countryIndex].languages).join(
+            ', '
+          )}</p>
+
+          `
+          );
           console.log(`Name: ${countries[countryIndex].name.official}`);
           console.log(`Capital: ${countries[countryIndex].capital}`);
           console.log(`Population: ${countries[countryIndex].population}`);
           console.log(`Flag: ${countries[countryIndex].flags.svg}`);
+          console.log(`Flag: ${countries.flags.svg}`);
           console.log(`Language: ${countries[countryIndex].languages.value}`);
           console.log(`Language: ${countries[countryIndex].languages}`);
           console.log(`countries length: ${countries.length}`);
@@ -38,6 +60,14 @@ function handleInput() {
         countries.forEach((country, countryIndex) => {
           console.log(`Flag: ${countries[countryIndex].flags.svg}`);
           console.log(`Name: ${countries[countryIndex].name.official}`);
+          countryList.insertAdjacentHTML(
+            'beforeend',
+            `<li class="country-list__item">
+                     <img class="country-list__image" src="${countries[countryIndex].flags.svg}" alt="${countries[countryIndex].flags.alt}" width="90" height="57"/>
+            <span class="country-list__name">${countries[countryIndex].name.official}</span> 
+            </li>
+          `
+          );
         });
       }
     })

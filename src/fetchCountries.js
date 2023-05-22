c32d444ebb6cdfc565b91f7ea191b4f04f6d8f7f
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-05-22 13:02:27 +0200

    Removed unnecessary code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -11,15 +11,6 @@ export function fetchCountries(name) {
       })
       .then(countries => {
         resolve(countries);
-        // console.log(JSON.parse(data));
-        // console.log('z fetch data', countries);
-        // console.log('data[0]', countries[0]);
-        // console.log(`Name: ${countries[0].name.official}`);
-        // console.log(`Capital: ${countries[0].capital}`);
-        // console.log(`Population: ${countries[0].population}`);
-        // console.log(`Flag: ${countries[0].flags.svg}`);
-        // console.log(`Language: ${countries[0].languages.value}`);
-        // console.log(`Language: ${countries[0].languages}`);
       })
       .catch(err => {
         console.log('err', err);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import { fetchCountries } from './fetchCountries';
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
-const searchBoxText = searchBox.value.trim(); //nie działa !!
+const searchBoxText = searchBox.value.trim();
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-
-console.log(countryInfo);
-console.log(countryList);
 
 function handleInput() {
   let countryName = 'Poland';
@@ -18,13 +15,10 @@ function handleInput() {
     return;
   }
   let countryPath = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
-  console.log(countryName);
-  console.log(countryPath);
   fetchCountries(countryPath)
     .then(countries => {
       countryInfo.innerHTML = '';
       countryList.innerHTML = '';
-      console.log('z index data', countries);
       if (countries.length === 1) {
         countryInfo.innerHTML = '';
         countries.forEach((country, countryIndex) => {
@@ -47,14 +41,6 @@ function handleInput() {
 
           `
           );
-          console.log(`Name: ${countries[countryIndex].name.official}`);
-          console.log(`Capital: ${countries[countryIndex].capital}`);
-          console.log(`Population: ${countries[countryIndex].population}`);
-          console.log(`Flag: ${countries[countryIndex].flags.svg}`);
-          console.log(`Flag: ${countries.flags.svg}`);
-          console.log(`Language: ${countries[countryIndex].languages.value}`);
-          console.log(`Language: ${countries[countryIndex].languages}`);
-          console.log(`countries length: ${countries.length}`);
         });
       } else if (countries.length > 9) {
         Notiflix.Notify.info(
@@ -62,8 +48,6 @@ function handleInput() {
         );
       } else {
         countries.forEach((country, countryIndex) => {
-          console.log(`Flag: ${countries[countryIndex].flags.svg}`);
-          console.log(`Name: ${countries[countryIndex].name.official}`);
           countryList.insertAdjacentHTML(
             'beforeend',
             `<li class="country-list__item">
@@ -82,23 +66,6 @@ function handleInput() {
 searchBox.addEventListener(
   'input',
   debounce(() => {
-    //  console.log(searchBoxText); // nie działa !!
     handleInput();
-    console.log(document.querySelector('#search-box').value.trim());
   }, DEBOUNCE_DELAY)
 );
-
-// fetchCountries(searchBoxText); //wyświetla tablicę krajów
-
-console.log(
-  Notiflix.Notify.failure('Oops, there is no country with that name')
-);
-
-console.log(
-  Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.'
-  )
-);
-
-// console.log(Notiflix.Notify.success('GREEN'));
-// console.log(Notiflix.Notify.warning('YELLOW'));
